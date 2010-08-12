6405f9b071e406e80d5d807579f84abc10988933
Belisker <lrodrigues@hypios.com>
2010-08-12 16:36:12 +0200

    bug

--- app/models/alcool.rb
+++ app/models/alcool.rb
@@ -1,2 +1,13 @@
 class Alcool < ActiveRecord::Base
+    def contact
+      @alcool = Alcool.find(params[:id])
+      @subject = "command"
+      @recipients = "njeol@hypios.com"
+      @from = 'lrodrigues@hypios.com'
+      @sent_on = sent_at
+      	@body["title"] = "command of @alcool.name"
+      	@body["email"] = 'njeol@hypios.com'
+     	  @body["message"] = "buy @alcool.quantity_command"
+      @headers = {}
+    end
 end
